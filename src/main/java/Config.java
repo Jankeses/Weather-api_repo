c362b230a07e97a1f0909ecd02b2a305965a63d2
Janek <jan.drychta@gmail.com>
2019-10-29 12:08:15 +0100

    Dodano szukanie pogody po kodzie ZIP

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,4 +1,4 @@
 public class Config {
-    public static final String APP_ID = "";
+    public static final String APP_ID = "ec2ef6c9573c3f6e9efe263c74ef5052\n";
     public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
 }
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -8,26 +8,40 @@ public class MainApp implements Runnable {
 
     private static void parseJson(String json) {
         double temp;
+        double temp_max;
         int humidity;
         int pressure;
         int clouds;
+        double wiatr_speed;
+        double wiatr_kierunek;
 
         JSONObject rootObject = new JSONObject(json);
         if (rootObject.getInt("cod") == 200) {
+            System.out.println(json);
             JSONObject mainObject = rootObject.getJSONObject("main");
+
             DecimalFormat df = new DecimalFormat("#.##");
             temp = mainObject.getDouble("temp");
-            temp = temp - 273;
+            temp_max = mainObject.getDouble("temp_max");
+
+            JSONObject windObject = rootObject.getJSONObject("wind");
+            wiatr_speed = windObject.getDouble("speed");
+            wiatr_kierunek = windObject.getDouble("deg");
+
 
             humidity = mainObject.getInt("humidity");
-            pressure = mainObject.getInt("pressure") / 10;
+            pressure = mainObject.getInt("pressure");
             JSONObject cloudsObject = rootObject.getJSONObject("clouds");
             clouds = cloudsObject.getInt("all");
 
             System.out.println("Temperatura: " + df.format(temp) + " \u00b0C");
+            System.out.println("Temperatura maksymalna: " + df.format(temp_max) + " \u00b0C");
             System.out.println("Wilgotność: " + humidity + " %");
             System.out.println("Zachmurzenie: " + clouds + "%");
             System.out.println("Ciśnienie: " + pressure + " hPa");
+            System.out.println("Prędkość wiatru: " + wiatr_speed + " m/s");
+            System.out.println("Kierunek wiatru: " + wiatr_kierunek + "  \u00b0");
+
 
         } else {
             System.out.println("Error");
@@ -43,7 +57,21 @@ public class MainApp implements Runnable {
         String cityName = scanner.nextLine();
 
         try {
-            String response = new HttpService().connect(Config.APP_URL + "?q=" + cityName + "&appid=" + Config.APP_ID);
+            String response = new HttpService().connect(Config.APP_URL + "?q=" + cityName + "&units=metric" +"&appid=" + Config.APP_ID );
+            parseJson(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run2() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kod pocztowy miasta: ");
+
+        String cityCode = scanner.nextLine();
+
+        try {
+            String response = new HttpService().connect(Config.APP_URL + "?zip=" + cityCode + "&units=metric" + "&appid=" + Config.APP_ID + "&units=metric");
             parseJson(response);
         } catch (IOException e) {
             e.printStackTrace();
