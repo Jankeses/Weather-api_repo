47767c3c8e353c27be4607057f0e12c5d34dc83c
Janek <jan.drychta@gmail.com>
2019-10-29 14:11:44 +0100

    Rozbicie na metody oraz zapętlenie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     costam();
+        MainApp app = new MainApp();
+        app.run();
        }
-       
-       public static void costam(){
-           MainApp apka = new MainApp();
-           Scanner scanner = new Scanner(System.in);
-           System.out.println("Podaj liczbę" +"\n" +
-                   "1 dla szukania miasta po nazwie" +
-                   "\n" + "2 dla szukania miasta po kodzie zip" +
-                   "\n" + "0 do wyłączenia aplikacji");
-           int runType = scanner.nextInt();
-           switch (runType){
-               case 1:
-                   apka.run();
-                   break;
-               case 2:
-                   apka.run2();
-                   break;
-               case 0:
-                   break;
-               default:
-                   System.out.println("Invalid number please try again");
 
-       }
-           costam();
-       }
+
 }
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -6,7 +6,7 @@ import org.json.JSONObject;
 
 public class MainApp implements Runnable {
 
-    private static void parseJson(String json) {
+    private void parseJson(String json) {
         double temp;
         double temp_max;
         int humidity;
@@ -46,50 +46,91 @@ public class MainApp implements Runnable {
         } else {
             System.out.println("Error");
         }
+        run();
     }
 
     @Override
     public void run() {
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę" + "\n" +
+                "1 dla szukania miasta po nazwie" +
+                "\n" + "2 dla szukania miasta po kodzie zip" +
+                "\n" + "3 dla szukania miasta po ID" +
+                "\n" + "4 dla szukania miasta po dlugosci i szerokisci geograficznej" +
+                "\n" + "0 do wyłączenia aplikacji");
+        int runType = scanner.nextInt();
+        switch (runType) {
+            case 1:
+                nameCity();
+                break;
+            case 2:
+                zipCity();
+                break;
+            case 3:
+                idCity();
+                break;
+            case 4:
+                latilonCity();
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Invalid number please try again");
+                run();
+
+        }
+    }
+
+    private void latilonCity() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj lat i lon miasta: ");
+        String lat = scanner.next();
+        String lon = scanner.next();
+        try {
+            String response = new HttpService().connect(Config.APP_URL + "?lat=" + lat + "&" + "?lon=" + lon + "&units=metric" + "&appid=" + Config.APP_ID);
+            parseJson(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void idCity() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj kod ID miasta: ");
+        String cityID = scanner.next();
+        try {
+            String response = new HttpService().connect(Config.APP_URL + "?id=" + cityID + "&units=metric" + "&appid=" + Config.APP_ID);
+            parseJson(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void zipCity() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj kod pocztowy miasta: ");
+        String cityCode = scanner.next();
+        try {
+            String response = new HttpService().connect(Config.APP_URL + "?zip=" + cityCode + ",pl" + "&units=metric" + "&appid=" + Config.APP_ID);
+            parseJson(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void nameCity() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Podaj nazwę miasta: ");
-
-        String cityName = scanner.nextLine();
-
+        String cityName = scanner.next();
         try {
-            String response = new HttpService().connect(Config.APP_URL + "?q=" + cityName + ",pl" + "&units=metric" +"&appid=" + Config.APP_ID );
-            parseJson(response);
 
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void run2() {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj kod pocztowy miasta: ");
-        String cityCode = scanner.nextLine();
-
-        try {
-            String response = new HttpService().connect(Config.APP_URL + "?zip=" + cityCode + ",pl" + "&units=metric" + "&appid=" + Config.APP_ID);
+            String response = new HttpService().connect(Config.APP_URL + "?q=" + cityName + ",pl" + "&units=metric" + "&appid=" + Config.APP_ID);
             parseJson(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void run3() {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj kod pocztowy miasta: ");
-        String cityCode = scanner.nextLine();
-
-        try {
-            String response = new HttpService().connect(Config.APP_URL + "?zip=" + cityCode + ",pl" + "&units=metric" + "&appid=" + Config.APP_ID);
-            parseJson(response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
